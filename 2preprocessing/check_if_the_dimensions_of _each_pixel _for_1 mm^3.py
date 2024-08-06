6251eea447988b0fdff2d88de65d9a95b4bcb4bf
koushik313 <48552429+koushik313@users.noreply.github.com>
2024-08-06 17:55:33 -0500

    Add files via upload

---
+++ 2preprocessing/check_if_the_dimensions_of _each_pixel _for_1 mm^3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import nibabel as nib
+
+folder_path = r'/data/ddg9492/code1/jupyter_notebook/lung_upenn/upenn_data/test_1mm'
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.nii.gz'):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            nii = nib.load(file_path)
+            sx, sy, sz = nii.header.get_zooms()
+            if abs(sx - 1.0) < 1e-20 and abs(sy - 1.0) < 1e-20 and abs(sz - 1.0) < 1e-20:
+                print(f"{filename}: Voxel sizes are approximately 1 mm³.")
+            else:
+                print(f"{filename}: Voxel sizes are not 1 mm³.")
+        except Exception as e:
+            print(f"Error loading {filename}: {str(e)}")
+
